febd3472c3ba0e69205aa15adc9a58b6dd4c6d3f
vvu060 <vvu060@gmail.com>
2024-01-07 13:29:47 +0530

    Add Navigation component

---
+++ Navigation.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Feed from './screens/Feed';
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Feed />
+    </NavigationContainer>
+  );
+}
