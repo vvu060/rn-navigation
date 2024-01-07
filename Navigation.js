2cef54adc20ff8c488244beda1302f5e9dcbc576
vvu060 <vvu060@gmail.com>
2024-01-07 14:22:59 +0530

    Refactor navigation to use bottom tab navigation

--- Navigation.js
+++ Navigation.js
@@ -1,10 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
-import Feed from './screens/Feed';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feed from './screens/tabScreens/Feed';
+
+// Tab bottom
+const Tab = createBottomTabNavigator();
+
+function TabGroup() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Feed' component={Feed} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Feed />
+      <TabGroup />
     </NavigationContainer>
   );
 }
