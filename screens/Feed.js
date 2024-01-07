5044d8ba1d8d64a9e379768c01cc6b3a53b208d7
vvu060 <vvu060@gmail.com>
2024-01-07 13:30:34 +0530

    Add Feed screen

---
+++ screens/Feed.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native';
+
+const Feed = () => {
+  return (
+    <SafeAreaView>
+      <Text>Feed</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Feed;
